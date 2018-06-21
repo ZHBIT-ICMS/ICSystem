043d0110b5896ec96b58f95701dc8ca8740ed1a3
孔文轩 <1195529413@qq.com>
2018-06-21 12:22:32 +0800

    增加通知信息管理信息管理Impl

--- src/com/zhbit/service/impl/NoticeServiceImpl.java
+++ src/com/zhbit/service/impl/NoticeServiceImpl.java
@@ -2,22 +2,39 @@ package com.zhbit.service.impl;
 
 import com.zhbit.dao.BaseDAO;
 import com.zhbit.entity.Notice;
+import com.zhbit.entity.base.DataGrid;
 import com.zhbit.entity.base.PageBean;
+import com.zhbit.entity.vo.VoNotice;
 import com.zhbit.service.NoticeService;
 import com.zhbit.util.StringUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by wby on 2018/5/28.
+ * modified by wenxuan
  */
 @Service("noticeService")
 public class NoticeServiceImpl implements NoticeService  {
     @Resource
     private BaseDAO<Notice> baseDAO;
+
+    public BaseDAO<Notice> getBaseDAO() {
+        return baseDAO;
+    }
+
+    public void setBaseDAO(BaseDAO<Notice> baseDAO) {
+        this.baseDAO = baseDAO;
+    }
+
     /**
      * չʾ���й��棬����չʾ1-8�����¹���
      * @param s_notice
@@ -44,6 +61,8 @@ public class NoticeServiceImpl implements NoticeService  {
 
 
     }
+
+
     /**
      * ����id �����������
      * @param noticeId
@@ -70,6 +89,99 @@ public class NoticeServiceImpl implements NoticeService  {
             }
             return (Long)baseDAO.count(hql.toString().replaceFirst("and", "where"), param);
         }
+
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    public DataGrid datagrid(VoNotice voNotice) {
+        DataGrid j = new DataGrid();
+        j.setRows(this.changeModel(this.find(voNotice)));
+        j.setTotal(total(voNotice));
+        return j;
+    }
+
+    /**
+     *将Notice转换成VoNotice（视图层模型）
+     */
+    private List<VoNotice> changeModel(List<Notice> noticeList) {
+        List<VoNotice> voNoticeList = new ArrayList<VoNotice>();
+        if (noticeList != null && noticeList.size() > 0) {
+            for (Notice tn : noticeList) {
+                VoNotice vn= new VoNotice();
+                BeanUtils.copyProperties(tn, vn);
+                voNoticeList.add(vn);
+            }
+        }
+        return voNoticeList;
+    }
+
+    private List<Notice> find(VoNotice voNotice) {
+        String hql = " from Notice t where 1=1 ";
+
+        List<Object> values = new ArrayList<Object>();
+        hql = addWhere(voNotice, hql, values);
+
+        if (voNotice.getSort() != null && voNotice.getOrder() != null) {
+            hql += " order by " + voNotice.getSort() + " " + voNotice.getOrder();
+        }
+        PageBean pageBean = new PageBean(voNotice.getPage(),voNotice.getRows());
+        return baseDAO.find(hql, values,  pageBean);
+    }
+
+    private String addWhere(VoNotice voNotice, String hql, List<Object> values) {
+        return hql;
+    }
+
+    /**
+     *计算通知公告信息总数
+     */
+    private Long total(VoNotice voNotice) {
+        String hql = "select count(*) from Notice t where 1=1 ";
+        List<Object> values = new ArrayList<Object>();
+        hql = addWhere(voNotice, hql, values);
+        return baseDAO.count(hql, values);
+    }
+
+    @Override
+    public void delete(String ids) {
+        if (ids != null) {
+            for (String id : ids.split(",")) {
+                Notice t = baseDAO.get(Notice.class, Integer.parseInt(id));
+                System.out.println("```````````````````````````````删除通知公告信息id为"+id);
+                if (t != null) {
+                    baseDAO.delete(t);
+                }
+            }
+        }
+
+    }
+
+    //增加通知公告信息
+    @Override
+    public void add(VoNotice voNotice) {
+        if (voNotice.getCreateTime() == null) {
+            voNotice.setCreateTime(new Date());
+        }
+        Notice t = new Notice();
+        BeanUtils.copyProperties(voNotice, t);
+        baseDAO.save(t);
+    }
+
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    public String getContentById(int id) {
+        Notice t = baseDAO.get(Notice.class, id);
+        return t.getContent();
+    }
+
+    //修改通知公告信息
+    @Override
+    public void update(VoNotice voNotice) {
+        Notice t = baseDAO.get(Notice.class,voNotice.getId());
+        if (t != null) {
+            t.setTitle(voNotice.getTitle());
+            t.setContent(voNotice.getContent());
+            t.setCreateTime(new Date());
+            t.setPeople(voNotice.getPeople());
+        }
+    }
 
 
 }
