dc3ca785909b14a8f666e5f28f608a5de4837c92
孔文轩 <1195529413@qq.com>
2018-06-21 12:20:44 +0800

    增加接口

--- src/com/zhbit/service/PolicyService.java
+++ src/com/zhbit/service/PolicyService.java
@@ -1,34 +1,53 @@
 package com.zhbit.service;
 
+import com.zhbit.entity.base.DataGrid;
 import com.zhbit.entity.base.PageBean;
 import com.zhbit.entity.Policy;
+import com.zhbit.entity.vo.VoPolicy;
 
 import java.util.List;
 
 /**
  * Created by wby on 2018/5/28.
+ * modified by wenxuan
  */
 //���߷���
 public interface PolicyService {
     /**
-     * չʾ���й��棬����չʾ1-8�����¹���
-     * @param s_policy
-     * @param pageBean
-     * @return
+     * 展示所有政策法规信息，
      */
-    public List<Policy>showPolicyList (Policy s_policy,PageBean pageBean);
+    public List<Policy> showPolicyList(Policy s_policy, PageBean pageBean);
 
     /**
-     * ����id ������߷�������
-     * @param policyId
-     * @return
+     * 根据id 查出政策法规信息详情
      */
-    public Policy getPolicyById(int policyId);
+    public Policy getPolicyById(int noticeId);
+
     /**
-     * �����ܼ�¼��
-     * @param s_policy
-     * @return
+     * 计算总记录数
      */
     public Long getPolicyCount(Policy s_policy);
 
+
+    /**
+     * 获得数据表格
+     */
+    public DataGrid datagrid(VoPolicy voPolicy);
+
+    /**
+     * 删除
+     */
+    public void delete(String ids);
+
+    /**
+     * 添加
+     */
+    public void add(VoPolicy voPolicy);
+
+    /**
+     * 获得政策法规信息内容
+     */
+    public String getContentById(int id);
+
+    public void update(VoPolicy voPolicy);
 }
